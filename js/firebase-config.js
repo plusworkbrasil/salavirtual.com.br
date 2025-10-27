b80936e959a06275aeefb2bdef7cf5d0e44525c4
plusworkbrasil <134965937+plusworkbrasil@users.noreply.github.com>
2025-10-27 19:15:13 -0300

    Replace Firebase config with new project settings
    Updated Firebase configuration with new project details.

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,16 +2,6 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js';
 import { getStorage } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js';
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAwDP21GlYLtYsfhA8rfWLhdYM3La08m4k",
-//   authDomain: "eduhub-51682.firebaseapp.com",
-//   projectId: "eduhub-51682",
-//   storageBucket: "eduhub-51682.appspot.com",
-//   messagingSenderId: "478655559211",
-//   appId: "1:478655559211:web:1baaf63e46e75964fb1c6e",
-//   measurementId: "G-N6H8TQH6QX"
-// };
-
 const firebaseConfig = {
   apiKey: "AIzaSyAnB-0u26pOsGDFzB-lWMw4fLRoac-W3Xg",
   authDomain: "salavirtual-aa697.firebaseapp.com",
